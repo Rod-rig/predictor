0d98ab6f8f34d014810d2cca1acacb97c596dc1a
Rod-rig <cherniakvadim08@gmail.com>
2019-04-08 23:35:01 +0300

    update cron script

--- server/crons/index.js
+++ server/crons/index.js
@@ -1,3 +1,17 @@
+require("dotenv").config();
 const { updateStatuses } = require("./update-statuses");
+const express = require("express");
+const router = require("../router");
+const app = express();
+const db = require("../db");
 
-updateStatuses();
+db.connect(process.env.DB_URL, err => {
+  if (err) {
+    return console.error(err);
+  }
+  router(app);
+  app.listen(process.env.PORT, () => {
+    console.log("Server started");
+    updateStatuses();
+  });
+});
