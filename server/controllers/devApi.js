da6b9a29a6a159b5d53462436c5d6e32b7b5c08c
Rod-rig <cherniakvadim08@gmail.com>
2019-07-05 23:00:30 +0300

    revert dev pred api

--- server/controllers/devApi.js
+++ server/controllers/devApi.js
@@ -3,7 +3,6 @@ const tableMock = require("../config/mocks/tableMock");
 const resultsMock = require("../config/mocks/resultsMock");
 const resultsByDateMock = require("../config/mocks/results-by-date-mock");
 const scheduleMock = require("../config/mocks/scheduleMock");
-const scheduleMocks = require("../config/mocks/scheduleMocks");
 
 module.exports.getAllTournaments = (req, res) => {
   res.status(200).send(tournamentListMock);
@@ -22,11 +21,5 @@ module.exports.getResultsByDate = (req, res) => {
 };
 
 module.exports.getSchedule = (req, res) => {
-  if (req.params.date === "2019-07-05") {
-    res.status(200).send({ sport_events: scheduleMock });
-  } else if (req.params.date === "2019-07-06") {
-    res.status(200).send({ sport_events: [] });
-  } else {
-    res.status(200).send({ sport_events: scheduleMocks });
-  }
+  res.status(200).send({ sport_events: scheduleMock });
 };
