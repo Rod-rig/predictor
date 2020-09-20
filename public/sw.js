2e04b5e29872e190a64af32972056cc4d3d6d948
Rod-rig <cherniakvadim08@gmail.com>
2020-09-20 20:14:09 +0300

    enable pwa

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const staticCacheName = "site-static-v1";
+const assets = ["/", "/dist/bundle.js", "/src/images/logo.svg"];
+
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    }),
+  );
+});
+
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName)
+          .map(key => caches.delete(key)),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    }),
+  );
+});
