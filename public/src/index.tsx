5a7725746e8b7abccaba533b6e6bb5ba26b3c145
Vadim <Rod-rig@users.noreply.github.com>
2020-09-20 20:21:11 +0300

    Enable pwa (#24)
    * enable pwa

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -2,4 +2,25 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/App/App";
 
+const registerServiceWorker = () => {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("./sw.js")
+        .then(registration => {
+          // tslint:disable-next-line:no-console
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope,
+          );
+        })
+        .catch(err => {
+          // tslint:disable-next-line:no-console
+          console.log("ServiceWorker registration failed: ", err);
+        });
+    });
+  }
+};
+
 ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
