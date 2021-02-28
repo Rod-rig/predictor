850102a8fdba53bdd5ec4cba313c77176e898d23
Rod-rig <cherniakvadim08@gmail.com>
2021-02-28 18:52:52 +0200

    add browser sync package

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,14 @@
 const config = require("./webpack.common.config");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = Object.assign({}, config, {
   mode: "development",
   devtool: "source-map",
+  plugins: [
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 8080,
+      proxy: "http://localhost:8080/",
+    }),
+  ],
 });
