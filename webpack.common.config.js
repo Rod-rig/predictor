c5b2cf82299409f29e334c311e8e0697de57210e
Vadim <Rod-rig@users.noreply.github.com>
2021-03-07 13:32:16 +0200

    add terser minify plugin (#33)

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,3 +1,4 @@
+const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -6,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "./public/dist"),
     filename: "bundle.js",
     publicPath: "/dist",
+    clean: true,
   },
   module: {
     rules: [
@@ -27,5 +29,19 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
     modules: ["node_modules"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
   },
 };
